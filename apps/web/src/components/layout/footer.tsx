936666d91afe83653b175a281876bf166819e0be
Naod Y. Aseffa <naod@blencorp.com>
2025-07-29 16:01:28 -0400

    chore: updated footer

--- apps/web/src/components/layout/footer.tsx
+++ apps/web/src/components/layout/footer.tsx
@@ -1,15 +1,12 @@
 export function Footer() {
-  const currentYear = new Date().getFullYear();
-
   return (
-    <footer className="padding-3 bg-base-darker text-white">
-      <div className="max-width-desktop margin-x-auto display-flex flex-align-center flex-justify-between">
-        <p className="margin-0 font-body-xs">
-          © {currentYear} Department of Veterans Affairs
-        </p>
-        <p className="margin-0 font-body-xs">
-          AI Assist v0.1.0 - Clinical Decision Support Tool
-        </p>
+    <footer className="padding-3 bg-primary-darker">
+      <div className="max-width-desktop margin-x-auto">
+        <img
+          alt="U.S. Department of Veterans Affairs"
+          className="height-6"
+          src="/images/va-footer-logo.svg"
+        />
       </div>
     </footer>
   );
