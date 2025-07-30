f3c735f4581b57307cf13c41865ddbf64900ae94
Naod Y. Aseffa <naod@blencorp.com>
2025-07-29 20:50:28 -0400

    chore: udpated header with patient context data

--- apps/web/src/components/layout/header.tsx
+++ apps/web/src/components/layout/header.tsx
@@ -1,20 +1,21 @@
 import {
-  ClinicianInitialsButton,
-  Header as VACDSHeader,
-} from '@department-of-veterans-affairs/clinical-design-system';
-import { useLayoutStore } from '@/stores/layout-store';
+	ClinicianInitialsButton,
+	Header as VACDSHeader,
+} from "@department-of-veterans-affairs/clinical-design-system";
+
+const SAMPLE_PATIENT = {
+	firstName: "John",
+	lastName: "Doe",
+	ssn: "6789",
+};
 
 export function Header() {
-  const leftCollapsed = useLayoutStore((state) => state.leftCollapsed);
-
-  return (
-    <VACDSHeader appName={leftCollapsed ? 'VA | AI assist' : ''}>
-      <div className="display-flex flex-align-center">
-        <ClinicianInitialsButton
-          ariaLabel="User menu for VA.NPUser1@example.com"
-          initials="VU"
-        />
-      </div>
-    </VACDSHeader>
-  );
+	return (
+		<VACDSHeader appName="VA | AI assist" patientContextProps={SAMPLE_PATIENT}>
+			<ClinicianInitialsButton
+				ariaLabel="User menu for VA.NPUser1@example.com"
+				initials="VU"
+			/>
+		</VACDSHeader>
+	);
 }
