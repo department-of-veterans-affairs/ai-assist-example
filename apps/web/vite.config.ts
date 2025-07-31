e0a8735f8aee45a4106c1995f1246f6623fff1ef
Jeff Wishnie <jeff@wishnie.org>
2025-07-31 13:16:50 -0700

    feat(dev): use env to configure API port (#3)
    - add AI_ASSIST_API_PORT to api/.env
    - update vite config to respect VITE_API_URL env var
    - remove sub-workspace config files that are ignored
    - let dev server port be configurable frorweb app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,21 +1,28 @@
-import path from 'node:path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path, { dirname } from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(process.cwd(), 'src'),
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8001',
-        changeOrigin: true,
+// Get the current file path
+const script_dir = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, script_dir);
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(script_dir, "src"),
       },
     },
-  },
+    server: {
+      port: Number(env.VITE_DEV_SERVER_PORT) || 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:8001",
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
