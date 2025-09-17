cc66d91cb05e94efc23f11eda8aab802e9a5e707
Julius Oh <juliusoh@gmail.com>
2025-09-17 10:08:48 -0700

    fix: config (#73)
    * fix: config
    
    * fix: config

--- apps/web/public/config.js
+++ apps/web/public/config.js
@@ -1,7 +1,7 @@
 window.env = {
   HOST_ENV: 'dev',
   PUBLIC_URL: '/ai-assist/',
-  AUTH_CLIENT_ID: 'Lighthouse_NP',
+  AUTH_CLIENT_ID: 'Lighthouse_Sandbox_3_NP',
   AUTH_SCOPES: 'launch fhirUser openid profile patient/Patient.read',
   AUTH_REDIRECT_URI: 'index.html',
   AUTH_PKCE_MODE: 'required',
